21ee58dcc78da12a7b37cbcbdf2fe94a8594df13
David Ponce <apotheosus@outlook.com>
2020-11-12 00:44:08 -0300

    Firebase

---
+++ app/src/main/java/com/optativa1/a960m/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.optativa1.a960m
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
